debf197cedaaa33f2fc70c20a27c97d69a06a9d6
Darin Alleman <darin.alleman@gmail.com>
2020-10-13 21:53:51 -0400

    Add upload progress percentage to loader

--- ClientApp/src/app/action-panel/upload/upload.component.ts
+++ ClientApp/src/app/action-panel/upload/upload.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { EventEmitter } from '@angular/core';
 import { FileService } from 'src/app/services/file.service';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -12,10 +12,11 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class UploadComponent {
   @Output() uploaded = new EventEmitter();
 
-  fileData: File = null;
-  previewUrl:any = null;
-  fileUploadProgress: string = null;
-  uploadedFilePath: string = null;
+  private fileData: File = null;
+  private previewUrl:any = null;
+  private fileUploadProgress: string = null;
+  private uploadedFilePath: string = null;
+  private uploadProgress: number  = 0;
   constructor(private http: HttpClient, private _file: FileService, private spinner: NgxSpinnerService) { }
 
   changed(fileInput) {
@@ -24,11 +25,16 @@ export class UploadComponent {
     const formData = new FormData();
     formData.append('file', this.fileData);
     this.spinner.show();
-    this.http.post<any>('/api/Upload', formData).subscribe(res => {
-          this.spinner.hide();
-          this._file.setFileId(res.fileId);
-          this.uploaded.emit("");
-        });
+    this.http.post<any>('/api/Upload', formData, {reportProgress: true, observe: 'events'}).subscribe(res => {
+          if (res.type === HttpEventType.Response) {
+            this.spinner.hide();
+            this._file.setFileId(res.body.fileId);
+            this.uploaded.emit("");
+          }
+          else if (res.type === HttpEventType.UploadProgress) {
+            this.uploadProgress = Math.round(100 * res.loaded / res.total);
+          }
+      });
   }
 
 }
